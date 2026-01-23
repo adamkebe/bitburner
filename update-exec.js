204e383c1af350616e69d8f65e08433152cb7d91
adamkebe <krukad@gmail.com>
2026-01-23 20:49:56 +0100

    Update update-exec.js

--- update-exec.js
+++ update-exec.js
@@ -3,12 +3,10 @@ export async function main(ns) {
   // TO DO: replace with an automatically generated text file and a for loop
 await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/update.js","update.js")
   await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/go.js","go.js")
-  /*
-  await ns.wget("","scandep.js")
-  await ns.wget("","scanloop.js")
-  await ns.wget("","deploy.js")
-  /*
-  /*
+  await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/scandep.js","scandep.js")
+  await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/scanloop.js","scanloop.js")
+  await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/findserver.js","findserver.js")
+  /*await ns.wget("","deploy.js")
   await ns.wget()
   await ns.wget()
   */
