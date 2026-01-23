f002b571e67fc9a48cec4f8a41d362949a4e14f6
adamkebe <krukad@gmail.com>
2026-01-23 18:46:00 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,8 @@
 /** @param {NS} ns */
+/* download this script in the terminal using:
+wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/update.js","update.js")
+then start the script to download all of the others
+*/
 export async function main(ns) {
-await ns.wget()
+await ns.wget("https://raw.githubusercontent.com/adamkebe/bitburner/refs/heads/main/update-exec.js","update-exec.js")
 }
