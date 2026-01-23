04d486ab4009a2a6597acb933ea9bf7adfde62b7
adamkebe <krukad@gmail.com>
2026-01-23 20:45:39 +0100

    Create findserver.js

---
+++ findserver.js
@@ -0,0 +1,30 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let fileContent = ns.read("serverdetails.txt");
+  let serverDetails = JSON.parse(fileContent);
+  let higherServer = "none";
+  let targetServer = "CSEC"
+  if (ns.args[0] != undefined) {
+    targetServer = ns.args[0]
+  }
+  let serverPath = [targetServer]
+  let i = 0
+  ns.tprint("looking for ", targetServer)
+  while (higherServer != "home") {
+    for (let i = 0; i < serverDetails.length; i++) {
+      if (serverDetails[i].includes(targetServer)) {
+        //ns.tprint("SERVER FOUND!")
+        higherServer = serverDetails[i][2]
+        serverPath.push(higherServer)
+        targetServer = higherServer
+        ns.tprint(targetServer)
+        break
+      }
+    }
+  }
+  /*while (higherServer != "home") {
+  }
+  ns.tprint("all servers found")
+  ns.tprint(serverPath)
+*/
+}
