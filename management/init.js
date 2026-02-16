5914f6d12c1a5c9f3b3cb69446c8d26ea97f7a50
adamkebe <krukad@gmail.com>
2026-02-16 17:31:06 +0100

    Update init.js

--- management/init.js
+++ management/init.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-ns.run(persistent.js)
-ns.run(go.js)
-ns.run(singularity-manager.js)
+ns.run("persistent.js")
+ns.run("go.js")
+ns.run("singularity-manager.js")
 }
