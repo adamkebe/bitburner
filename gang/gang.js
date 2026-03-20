98e5bd351f0115bef56f96230c74fd1978062023
adamkebe <krukad@gmail.com>
2026-03-20 23:04:52 +0100

    Update gang.js

--- gang/gang.js
+++ gang/gang.js
@@ -1,4 +1,18 @@
 /** @param {NS} ns */
 export async function main(ns) {
-
+// gets all members
+  let members = ns.gang.getMemberNames()
+  // recruits new members
+let recruits = ns.gang.getRecruitsAvailable()
+  if (recruits>0) {
+for (let i = 0; i < recruits - 1; i++) {
+        ns.gang.ascendMember(members[members.length-1])
+      }
+  }
+  //ascends all members if they meet a certain condition 
+for (let i = 0; i < members.length; i++) {
+      if (true) {
+        ns.gang.ascendMember(members[i])
+      }
+  
 }
