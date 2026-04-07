648fe702f32ddff9f1359c4cc6d71e4dc2430d80
adamkebe <krukad@gmail.com>
2026-04-07 17:54:37 +0200

    Update gang.js
    Add loop, fixed naming issue, added Train Combat as first task, made ascension dependent on max stat ratio being at least 1.05.

--- gang/gang.js
+++ gang/gang.js
@@ -1,21 +1,32 @@
 /** @param {NS} ns */
 export async function main(ns) {
-// gets all members
+
+  while(true) {
+  // gets all members
   let members = ns.gang.getMemberNames()
   // recruits new members
 let recruits = ns.gang.getRecruitsAvailable()
   if (recruits>0) {
 for (let i = 0; i < recruits - 1; i++) {
        members
-  let name = members[members.length-1]+1;
+  if(members.length>0) {
+    let name = members[members.length-1]+1;
   ns.gang.recruitMember(name)
+  }
+  else {
+    let name = "1"
+ns.gang.recruitMember(name)
+  }
+ns.gang.setMemberTask(name, "Train Combat")
   
 }
   }
   //ascends all members if they meet a certain condition 
 for (let i = 0; i < members.length; i++) {
-      if (true) {
+      if (math.max(ns.gang.getAscenscionResult(members[i]))>1.05) {
         ns.gang.ascendMember(members[i])
       }
 }
+    ns.gang.nextUpdate()
+  } //end of while loop
 }
