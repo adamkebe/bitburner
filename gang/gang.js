23eff21d03cf46305c412ba0c2408d6f11cb44c3
adamkebe <krukad@gmail.com>
2026-04-07 22:00:48 +0200

    Update gang.js ascension

--- gang/gang.js
+++ gang/gang.js
@@ -26,8 +26,15 @@ ns.gang.setMemberTask(name, "Train Combat")
 for (let i = 0; i < members.length; i++) {
   ns.print(members[i])
   ns.print(ns.gang.getAscensionResult(members[i]))
-  ns.print(Math.max(ns.gang.getAscensionResult(members[i])))
-      if (Math.max(ns.gang.getAscensionResult(members[i]))>1.05) {
+  let hack = ns.gang.getAscensionResult(members[i]).hack
+  let strength = ns.gang.getAscensionResult(members[i]).str
+  let defence = ns.gang.getAscensionResult(members[i]).def
+  let dexterity = ns.gang.getAscensionResult(members[i]).dex
+  let agility = ns.gang.getAscensionResult(members[i]).agi
+  let charisma = ns.gang.getAscensionResult(members[i]).cha
+  let ascensionRatios = [hack, strength, defence, dexterity, agility, charisma]
+  ns.print(Math.max(ascensionRatios)
+      if (Math.max(ascensionRatios)>1.05) {
         ns.gang.ascendMember(members[i])
       }
 }
