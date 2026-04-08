97631739fcb25ab61d082a5372a01308f24428d4
adamkebe <krukad@gmail.com>
2026-04-08 13:19:03 +0200

    Create augment.js

---
+++ singularity/augment.js
@@ -0,0 +1,18 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  // set faction
+  let faction = "Slum Snakes"
+  // initial loop waits for augmentation to be available 
+  while("no augment") {
+    // check for augmentation 
+    ns.await(500)
+  }
+  while("augment available") {
+    // buy most expensive augmentation 
+}
+  //install augmentations 
+  ns.singularity.exportGame()
+  
+//ns.run("install.js",1,"augment")
+ns.exit()
+}
