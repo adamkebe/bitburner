e152bce1b4a7aaee4a13e5b46ba668f14734536c
adamkebe <krukad@gmail.com>
2026-04-17 18:13:03 +0200

    Update augment.js

--- singularity/augment.js
+++ singularity/augment.js
@@ -2,15 +2,20 @@
 export async function main(ns) {
   // set faction
   let faction = "Slum Snakes"
+  let debug = true
   // initial loop waits for augmentation to be available 
+ // All augs
+ let allAugs = ns.singularity.getAugmentationsFromFaction(faction)
+ if(debug) {
+ ns.print(allAugs) 
+ }
  /*
- All augs ns.singularity.getAugmentationsFromFaction()
  owned augs
  available augs
 
 
 
- */
+ 
   while("no augment") {
   // check for augmentation 
     ns.await(500)
@@ -22,5 +27,6 @@ export async function main(ns) {
   ns.singularity.exportGame()
   
 //ns.run("install.js",1,"augment")
+*/
 ns.exit()
 }
